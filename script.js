d595c3e04717c87e799a0d1994e854c5a510f7e3
NaratChheak <nararstore@gmail.com>
2025-07-07 10:29:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function downloadApp1() {
+  alert("កំពុងដោនឡូត iOS v1...");
+  // window.location.href = "https://example.com/ios-v1-download";
+}
+
+function downloadApp2() {
+  alert("កំពុងដោនឡូត iOS v2...");
+  // window.location.href = "https://example.com/ios-v2-download";
+}
